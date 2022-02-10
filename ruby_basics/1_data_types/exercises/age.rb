9fa303f62390717685d7190382b480e6c301966f
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-09 21:17:44 -0600

    add'l coding practice

---
+++ ruby_basics/1_data_types/exercises/age.rb
@@ -0,0 +1,4 @@
+puts "how old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you will be "
+puts age + 10
---
+++ ruby_basics/1_data_types/exercises/name.rb
@@ -0,0 +1,17 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+full_name = first_name + " " + last_name
+puts "your full name is"
+puts full_name
+
+10.times do
+    puts full_name
+end
+
+def numbers
+    p 123
+end
+
+numbers
